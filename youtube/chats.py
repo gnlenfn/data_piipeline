e83ea32afe0eb206f38954bad7bc6d158cdab6a2
gnlenfn <akenfn513@gmail.com>
2023-04-26 21:05:48 +0900

    feat: logging file and console

--- youtube/chats.py
+++ youtube/chats.py
@@ -5,22 +5,41 @@ import json
 import time
 from dotenv import load_dotenv
 import os
-from logging import handlers
 import logging 
+from logging import handlers
+import asyncio
 
 from google.cloud import storage
 
-CURRENT_PATH = os.getcwd()
-load_dotenv(f"{CURRENT_PATH}/youtube/env/.env")
+# logging
+CURRENT_PATH = os.path.dirname(os.path.realpath(__file__))
+CURRENT_FILE = os.path.basename(__file__)
+LOG_FILENAME = f"log-{CURRENT_FILE[:-3]}"
+LOG_DIR = f"{CURRENT_PATH}/logs"
+if not os.path.exists(LOG_DIR):
+    os.makedirs(LOG_DIR)
 
-log_handler = handlers.TimedRotatingFileHandler(filename=f"{CURRENT_PATH}/youtube/logs/chat.log", when='midnight', interval=1, encoding='utf-8')
-log_handler.setFormatter(logging.Formatter("%(asctime)s %(levelname)s:%(message)s"))
-log_handler.suffix = "%Y-%m-%d"
+load_dotenv(f"{CURRENT_PATH}/env/.env")
 
 logger = logging.getLogger()
 logger.setLevel(logging.WARNING)
-logger.addHandler(log_handler)
+formatter = logging.Formatter("%(asctime)s %(levelname)s:%(message)s")
 
+file_log_handler = handlers.TimedRotatingFileHandler(
+    filename=f"{CURRENT_PATH}/logs/{LOG_FILENAME}", when='midnight', interval=1, encoding='utf-8'
+    )
+file_log_handler.suffix = ".log-%Y-%m-%d"
+file_log_handler.setLevel(logging.INFO)
+file_log_handler.setFormatter(formatter)
+
+console_log_handler = logging.StreamHandler()
+console_log_handler.setLevel(logging.INFO)
+console_log_handler.setFormatter(formatter)
+
+logger.addHandler(file_log_handler)
+logger.addHandler(console_log_handler)
+
+# environment varialbes
 GCS_KEY_PATH = os.environ["GCS_KEY_PATH"]
 BUCKET_NAME = "2023-de-zoomcamp"
 FILE_NAME = "news_ytn_youtube"
@@ -35,35 +54,41 @@ file_path = './news_ytn_youtube.parquet'
 
 def scrape_chats() -> None:
     chat = pytchat.create(video_id=video_id)
-    df = pd.DataFrame()
+    df = pd.DataFrame(columns=['id', 'datetime', 'name', 'message', 'author_channel', 'is_chat_moderator'])
 
     while chat.is_alive():
-
         data = chat.get()
         items = data.items
 
         for c in items:
-            # print(f"{c.datetime} [{c.author.name}]- {c.message}")
             data.tick()
             j = json.loads(c.json())
-            df = pd.concat([df, pd.json_normalize(j)], ignore_index=True)
+            new_row = {
+                "id": j['id'],
+                "datetime": j['datetime'],
+                "message": j['message'],
+                "name": j['author']['name'],
+                "author_channel": j["author"]['channelId'],
+                "is_chat_moderator": j['author']['isChatModerator']
+            }
+            df.loc[len(df)] = new_row
 
-        if len(df) == 10000: 
-            save_df_to_gcs(df)
-            df = pd.DataFrame()
+        if len(df) >= 5: 
+            asyncio.run(save_df_to_gcs(df))
+            df = pd.DataFrame(columns=['id', 'datetime', 'name', 'message', 'author_channel', 'is_chat_moderator'])
 
     if len(df):
-        save_df_to_gcs(df)
+        asyncio.run(save_df_to_gcs(df))
 
 
-def save_df_to_gcs(df: pd.DataFrame) -> None:
+async def save_df_to_gcs(df: pd.DataFrame) -> None:
     df = df.astype(str)
     df.to_parquet(file_path)
 
     # upload to GCS
     storage_client = storage.Client()
     bucket = storage_client.bucket(BUCKET_NAME)
-    blob = bucket.blob(f"streaming_chat/{FILE_NAME}_{time.time()}.parquet")
+    blob = bucket.blob(f"streaming_chat/{FILE_NAME}_{int(time.time())}.parquet")
     blob.upload_from_filename(f"./{FILE_NAME}.parquet")
 
     logger.warning(f"{len(df)} length parquet uploaded!")
