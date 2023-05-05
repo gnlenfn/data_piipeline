3f587a2e6448271003231767592a20d3a7ea7e3b
gnlenfn <akenfn513@gmail.com>
2023-05-05 19:52:35 +0900

    feat: read parquet from local and handling df

--- spark/spark_data_transformation.py
+++ spark/spark_data_transformation.py
@@ -1,109 +1,40 @@
 from pyspark.sql import SparkSession
+from pyspark.sql import functions as F
 from pyspark.sql.types import StructField, StructType, LongType, TimestampType, DoubleType, StringType, IntegerType
 
-from config.default import *
+
 
 spark = (
     SparkSession
     .builder 
-    .appName("NYC TLC")
+    .master("local[*]")
+    .appName("test")
     .getOrCreate()
     )
 
-# for t in ['yellow', 'green', 'fhvhv']:
-#     df = spark.read.parquet(f"spark/data/input/{t}/2020/{t}_tripdata_2020-01.parquet")
-#     df.printSchema()
-#     df.show()
+df_green = spark.read.parquet('spark/data/input/green/*/*')
+df_green = df_green.withColumnRenamed('lpep_pickup_datetime', "pickup_datetime") \
+                    .withColumnRenamed('lpep_dropoff_datetime', "dropoff_datetime")
+df_green.printSchema()
 
-# schema of data
-yellow_schema = StructType([
-    StructField("VendorID", LongType(), True),
-    StructField("tpep_pickup_datetime", TimestampType(), True),
-    StructField("tpep_dropoff_datetime", TimestampType(), True),
-    StructField("passenger_count", DoubleType(), True),
-    StructField("trip_distance", DoubleType(), True),
-    StructField("RatecodeID", DoubleType(), True),
-    StructField("store_and_fwd_flag", StringType(), True),
-    StructField("PULocationID", LongType(), True),
-    StructField("DOLocationID", LongType(), True),
-    StructField("payment_type", LongType(), True),
-    StructField("fare_amount", DoubleType(), True),
-    StructField("extra", DoubleType(), True),
-    StructField("mta_tax", DoubleType(), True),
-    StructField("tip_amount", DoubleType(), True),
-    StructField("tolls_amount", DoubleType(), True),
-    StructField("improvement_surcharge", DoubleType(), True),
-    StructField("total_amount", DoubleType(), True),
-    StructField("congestion_surcharge", DoubleType(), True)
-])
+df_yellow = spark.read.parquet('spark/data/input/yellow/*/*')
+df_yellow = df_yellow.withColumnRenamed('tpep_pickup_datetime', "pickup_datetime") \
+                    .withColumnRenamed('tpep_dropoff_datetime', "dropoff_datetime")
+df_yellow.printSchema()
 
-green_schema = StructType([
-    StructField("VendorID", LongType(), True),
-    StructField("lpep_pickup_datetime", TimestampType(), True),
-    StructField("lpep_dropoff_datetime", TimestampType(), True),
-    StructField("store_and_fwd_flag", StringType(), True),
-    StructField("RatecodeID", DoubleType(), True),
-    StructField("PULocationID", LongType(), True),
-    StructField("DOLocationID", LongType(), True),
-    StructField("passenger_count", DoubleType(), True),
-    StructField("trip_distance", DoubleType(), True),
-    StructField("fare_amount", DoubleType(), True),
-    StructField("extra", DoubleType(), True),
-    StructField("mta_tax", DoubleType(), True),
-    StructField("tip_amount", DoubleType(), True),
-    StructField("tolls_amount", DoubleType(), True),
-    StructField("ehail_fee", IntegerType(), True),
-    StructField("improvement_surcharge", DoubleType(), True),
-    StructField("total_amount", DoubleType(), True),
-    StructField("payment_type", DoubleType(), True),
-    StructField("trip_type", DoubleType(), True),
-    StructField("congestion_surcharge", DoubleType(), True)
-])
+# select common columns
+common_columns = []
+yellow_columns = set(df_yellow.columns)
 
-fhvhv_schema = StructType([
-    StructField("hvfhs_license_num", StringType(), True),
-    StructField("dispatching_base_num", StringType(), True),
-    StructField("originating_base_num", StringType(), True),
-    StructField("request_datetime", TimestampType(), True),
-    StructField("on_scene_datetime", TimestampType(), True),
-    StructField("pickup_datetime", TimestampType(), True),
-    StructField("dropoff_datetime", TimestampType(), True),
-    StructField("PULocationID", LongType(), True),
-    StructField("DOLocationID", LongType(), True),
-    StructField("trip_miles", DoubleType(), True),
-    StructField("trip_time", LongType(), True),
-    StructField("base_passenger_fare", DoubleType(), True),
-    StructField("tolls", DoubleType(), True),
-    StructField("bcf", DoubleType(), True),
-    StructField("sales_tax", DoubleType(), True),
-    StructField("congestion_surcharge", DoubleType(), True),
-    StructField("airport_fee", DoubleType(), True),
-    StructField("tips", DoubleType(), True),
-    StructField("driver_pay", DoubleType(), True),
-    StructField("shared_request_flag", StringType(), True),
-    StructField("shared_match_flag", StringType(), True),
-    StructField("access_a_ride_flag", StringType(), True),
-    StructField("wav_request_flag", StringType(), True),
-    StructField("wav_match_flag", StringType(), True),
-])
+for col in df_green.columns:
+    if col in yellow_columns:
+        common_columns.append(col)
 
-year = 2020
-type = 'yellow'
-# for type, schema in zip(['yellow', 'green', 'fhvhv'], [yellow_schema, green_schema, fhvhv_schema]):
-for month in range(1, 13):
-    input_path = f"spark/data/input/{type}/{year}/"
-    output_path = f"spark/data/output/{type}/{year}/{month:02d}/"
+df_green_com = df_green.select(common_columns).withColumn('service_type', F.lit('green'))
+df_yellow_com = df_yellow.select(common_columns).withColumn('service_type', F.lit('yellow'))
+data = df_green_com.unionAll(df_yellow_com)
 
-    df = spark.read.option('header', 'true') \
-        .schema(yellow_schema) \
-        .parquet(input_path)
-    
-    df.repartition(20) \
-        .write \
-        .mode('overwrite') \
-        .parquet(output_path)
-
-
+data.groupBy('service_type').count().show()
 
 """TODO
 - 다운받은 데이터를 조작하여 원하는 부분만 GCS로 저장
