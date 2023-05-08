2672dd0829a4dbba74e597223d6a524b9536b0fe
gnlenfn <akenfn513@gmail.com>
2023-05-08 21:15:27 +0900

    feat: mysql 2 tables

--- spark/spark_data_transformation.py
+++ spark/spark_data_transformation.py
@@ -11,7 +11,6 @@
 
 from pyspark.sql import SparkSession
 from pyspark.sql import functions as F
-from pyspark.sql.types import StructField, StructType, LongType, TimestampType, DoubleType, StringType, IntegerType
 
 
 spark = (
@@ -19,19 +18,17 @@ spark = (
     .builder 
     .master("local[*]")
     .appName("test")
-    .config('spark.jars', "spark/config/mysql-connector-j-8.0.32.jar")
+    .config('spark.jars', "mysql-connector-j-8.0.32.jar")
     .getOrCreate()
     )
 
 df_green = spark.read.parquet('spark/data/input/green/*/*')
 df_green = df_green.withColumnRenamed('lpep_pickup_datetime', "pickup_datetime") \
                     .withColumnRenamed('lpep_dropoff_datetime', "dropoff_datetime")
-# df_green.printSchema()
 
 df_yellow = spark.read.parquet('spark/data/input/yellow/*/*')
 df_yellow = df_yellow.withColumnRenamed('tpep_pickup_datetime', "pickup_datetime") \
                     .withColumnRenamed('tpep_dropoff_datetime', "dropoff_datetime")
-# df_yellow.printSchema()
 
 # select common columns
 common_columns = []
@@ -45,6 +42,7 @@ df_green_com = df_green.select(common_columns).withColumn('service_type', F.lit(
 df_yellow_com = df_yellow.select(common_columns).withColumn('service_type', F.lit('yellow'))
 data = df_green_com.unionAll(df_yellow_com)
 data = data.filter(data.pickup_datetime >= '2020-01-01')  # 과거 데이터가 섞여있었음
+data = data.filter(data.pickup_datetime < '2023-01-01')   # 이상값 제거
 
 # data.groupBy('service_type').count().show()
 """
@@ -52,10 +50,11 @@ data = data.filter(data.pickup_datetime >= '2020-01-01')  # 과거 데이터가 
 뉴욕 전역에서 서비스하는 yellow taxi의 건수가 10배 이상
 """
 
+# Transform data with SQL
 data.createOrReplaceTempView("trip_data")  # sql 사용을 위한 테이블 등록
-monthly_agg = spark.sql("""
+monthly_yellow_agg = spark.sql("""
 SELECT
-    DATE_FORMAT(date_trunc('month', pickup_datetime), '%y-%M') AS month,
+    LEFT(pickup_datetime, 7) AS month,
     service_type,
 
     ROUND(SUM(fare_amount), 3) AS monthly_fare,
@@ -76,14 +75,48 @@ ORDER BY
     1 ASC
 """)
 
-monthly_agg.show()
+monthly_green_agg = spark.sql("""\
+SELECT
+    LEFT(pickup_datetime, 7) AS month,
+    service_type,
 
-monthly_agg.write \
+    ROUND(SUM(fare_amount), 3) AS monthly_fare,
+    ROUND(SUM(tip_amount), 3) AS monthly_tip,
+    ROUND(SUM(tolls_amount), 3) AS monthly_tolls,
+    ROUND(SUM(mta_tax), 3) AS monthly_mta_tax,
+    ROUND(SUM(congestion_surcharge), 3) AS monthly_congestion_charge,
+
+    ROUND(AVG(passenger_count), 3) AS avg_monthly_passengers,
+    ROUND(AVG(trip_distance), 3) AS avg_monthly_trip_distance
+FROM
+    trip_data
+WHERE
+    service_type = 'green'
+GROUP BY
+    1, 2
+ORDER BY
+    1 ASC
+""")
+
+# Load to MySQL
+monthly_yellow_agg.write \
     .format("jdbc") \
     .mode('overwrite') \
     .option("driver", "com.mysql.cj.jdbc.Driver") \
     .option("url", "jdbc:mysql://localhost:3306/pipeline?useSSL=false&allowPublicKeyRetrieval=true") \
-    .option("dbtable", "monthly_revenue") \
+    .option("dbtable", "monthly_yellow_revenue") \
     .option("user", "root") \
     .option("password", "root") \
     .save()
+
+monthly_green_agg.write \
+    .format("jdbc") \
+    .mode('overwrite') \
+    .option("driver", "com.mysql.cj.jdbc.Driver") \
+    .option("url", "jdbc:mysql://localhost:3306/pipeline?useSSL=false&allowPublicKeyRetrieval=true") \
+    .option("dbtable", "monthly_green_revenue") \
+    .option("user", "root") \
+    .option("password", "root") \
+    .save()
+
+# input("Waiting...")  # to check on web UI
